73218e2adc1f2f3e90b162aaf050af9c9eaca955
Thomas Patton <tjpatton1@gmail.com>
2024-01-17 17:01:04 -0800

    cleanup ensure

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -23,14 +23,6 @@ M.mason = {
     -- lua stuff
     "lua-language-server",
     "stylua",
-
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
-
     -- c/cpp stuff
     "clangd",
     "clang-format",
