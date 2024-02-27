254f03857466247e6edce311cc07cd65bf6ab11a
Thomas Patton <tjpatton1@gmail.com>
2024-02-26 22:29:13 -0800

    restart mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,6 +5,7 @@ M.general = {
     n = {
         ["<leader>rn"] = { "<cmd> lua vim.lsp.buf.rename() <CR>", "Vim LSP Rename" },
         ["<leader>fmt"] = { "<cmd> lua vim.lsp.buf.format() <CR>", "Vim LSP Format" },
+        ["<leader>lsp"] = { "<cmd> LspRestart <CR>", "Vim LSP Restart" },
     },
     v = {
         [">"] = { ">gv", "indent"},
