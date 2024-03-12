ed0037319bfeaff6bd671eff1963a1f45558fc6b
Thomas Patton <tjpatton1@gmail.com>
2024-03-11 19:03:30 -0700

    add additional mappings for jk leader

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -6,6 +6,8 @@ M.general = {
         ["<leader>rn"] = { "<cmd> lua vim.lsp.buf.rename() <CR>", "Vim LSP Rename" },
         ["<leader>fmt"] = { "<cmd> lua vim.lsp.buf.format() <CR>", "Vim LSP Format" },
         ["<leader>lsp"] = { "<cmd> LspRestart <CR>", "Vim LSP Restart" },
+        ["<leader>j"] = { "5j", "Move 5 lines down" }, -- Added line for leader j
+        ["<leader>k"] = { "5k", "Move 5 lines up" }, -- Added line for leader k
     },
     v = {
         [">"] = { ">gv", "indent"},
