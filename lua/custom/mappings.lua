a489806399987c21ebc633df3de53e1377bff0a0
Thomas Patton <tjpatton1@gmail.com>
2024-06-12 09:46:42 -0400

    copilot?

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -14,4 +14,16 @@ M.general = {
     },
 }
 
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+       {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+      }
+  }
+}
+
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -48,7 +48,19 @@ local plugins = {
   },
   {
     'simrat39/rust-tools.nvim'
-  }
+  },
+  -- copilot!
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()  -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+    -- The mapping is set to other key, see custom/lua/mappings
+    -- or run <leader>ch to see copilot mapping section
+    end
+  },
 
   -- To make a plugin not be loaded
   -- {
