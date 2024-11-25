5b9050addae2001f51ac5cc582ce4eb1a303e850
Thomas Patton <tjpatton1@gmail.com>
2024-11-24 19:14:50 -0800

    yeah yeah yeah yeah yeah yeah

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -34,109 +34,109 @@ return {
     },
 
     -- nvim-cmp for autocompletion
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "saadparwaiz1/cmp_luasnip",
-        "L3MON4D3/LuaSnip",
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "saadparwaiz1/cmp_luasnip",
+            "L3MON4D3/LuaSnip",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            -- Define border
+            local function border(hl_name)
+                return {
+                    { "╭", hl_name },
+                    { "─", hl_name },
+                    { "╮", hl_name },
+                    { "│", hl_name },
+                    { "╯", hl_name },
+                    { "─", hl_name },
+                    { "╰", hl_name },
+                    { "│", hl_name },
+                }
+            end
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = {
+                        border = border("CmpBorder"),
+                        winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
+                        scrollbar = false,
+                    },
+                    documentation = {
+                        border = border("CmpDocBorder"),
+                        winhighlight = "Normal:CmpDoc",
+                    },
+                },
+                mapping = {
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.close(),
+                    ["<CR>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                },
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                }),
+            })
+
+            -- Command line completions
+            cmp.setup.cmdline(":", {
+                sources = {
+                    { name = "path" },
+                    { name = "cmdline" },
+                },
+            })
+        end,
     },
-    config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
 
-        -- Define border
-        local function border(hl_name)
-            return {
-                { "╭", hl_name },
-                { "─", hl_name },
-                { "╮", hl_name },
-                { "│", hl_name },
-                { "╯", hl_name },
-                { "─", hl_name },
-                { "╰", hl_name },
-                { "│", hl_name },
-            }
-        end
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            window = {
-                completion = {
-                    border = border("CmpBorder"),
-                    winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
-                    scrollbar = false,
-                },
-                documentation = {
-                    border = border("CmpDocBorder"),
-                    winhighlight = "Normal:CmpDoc",
-                },
-            },
-            mapping = {
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            },
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-            }),
-        })
-
-        -- Command line completions
-        cmp.setup.cmdline(":", {
-            sources = {
-                { name = "path" },
-                { name = "cmdline" },
-            },
-        })
-    end,
-},
-
-      -- LSP Configuration
-      {
-          "williamboman/mason.nvim",
-          config = function()
-              require("mason").setup()
-          end,
-      },
-      {
-          "williamboman/mason-lspconfig.nvim",
-          dependencies = { "neovim/nvim-lspconfig" },
-          config = function()
-              require("mason-lspconfig").setup({
-                  ensure_installed = { "terraformls", "yamlls", "pyright", "gopls"},
-              })
-          end,
-      },
+    -- LSP Configuration
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "neovim/nvim-lspconfig" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "terraformls", "yamlls", "pyright", "gopls"},
+            })
+        end,
+    },
 
     -- Telescope for fuzzy finding
     {
@@ -146,29 +146,29 @@ return {
         config = function()
             require("telescope").setup {
                 defaults = {
-                  vimgrep_arguments = {
-                      "rg",
-                      "-L",
-                      "--color=never",
-                      "--no-heading",
-                      "--with-filename",
-                      "--line-number",
-                      "--column",
-                      "--smart-case",
+                    vimgrep_arguments = {
+                        "rg",
+                        "-L",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
                     },
                     layout_config = {
-                          horizontal = {
+                        horizontal = {
                             prompt_position = "top",
                             preview_width = 0.55,
                             results_width = 0.8,
-                          },
-                          vertical = {
-                            mirror = false,
-                          },
-                          width = 0.87,
-                          height = 0.80,
-                          preview_cutoff = 120,
                         },
+                        vertical = {
+                            mirror = false,
+                        },
+                        width = 0.87,
+                        height = 0.80,
+                        preview_cutoff = 120,
+                    },
                     sorting_strategy = "ascending", -- Ascend through results (Tab moves down)
                     mappings = {
                         i = {
@@ -181,18 +181,35 @@ return {
         end,
     },
 
+    -- Project.nvim for project management
+    {
+        "ahmedkhalf/project.nvim",
+        config = function()
+            require("project_nvim").setup({
+                detection_methods = { "lsp", "pattern" },
+                patterns = { ".git", "Makefile", "package.json", "pyproject.toml" },
+            })
+            require("telescope").load_extension("projects")
+            -- Keybinding for quick project access
+            vim.api.nvim_set_keymap(
+                "n",
+                "<leader>fp",
+                ":Telescope projects<CR>",
+                { noremap = true, silent = true }
+            )
+        end,
+    },
+
     -- Copilot integration
     {
         "github/copilot.vim",
         lazy = false,
         config = function()  -- Mapping tab is already used by NvChad
-          vim.g.copilot_no_tab_map = true;
-          vim.g.copilot_assume_mapped = true;
-          vim.g.copilot_tab_fallback = "";
-        -- The mapping is set to other key, see custom/lua/mappings
-        -- or run <leader>ch to see copilot mapping section
-        end
-      },
+            vim.g.copilot_no_tab_map = true
+            vim.g.copilot_assume_mapped = true
+            vim.g.copilot_tab_fallback = ""
+        end,
+    },
 
     -- i love cats
     {
@@ -225,35 +242,28 @@ return {
     },
 
     {
-      "lewis6991/gitsigns.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-        require("gitsigns").setup({})
-      end,
+        "lewis6991/gitsigns.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("gitsigns").setup({})
+        end,
     },
 
     {
-      "ggandor/leap.nvim",
-      config = function()
-          require('leap').add_default_mappings()
-      end,
+        "ggandor/leap.nvim",
+        config = function()
+            require("leap").add_default_mappings()
+        end,
     },
 
-  {
     {
-      "CopilotC-Nvim/CopilotChat.nvim",
-      branch = "canary",
-      dependencies = {
-        { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-        { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-      },
-      build = "make tiktoken", -- Only on MacOS or Linux
-      opts = {
-        -- See Configuration section for options
-      },
-      -- See Commands section for default commands if you want to lazy load on them
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            { "github/copilot.vim" },
+            { "nvim-lua/plenary.nvim" },
+        },
+        build = "make tiktoken",
+        opts = {},
     },
-  }
 }
-
-
