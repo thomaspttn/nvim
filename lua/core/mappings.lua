99cafe3220bafbb4446d0f8a38cc907c4379fcaf
Thomas Patton <tjpatton1@gmail.com>
2025-04-15 14:12:21 -0400

    more changes

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -9,19 +9,18 @@ vim.g.maplocalleader = " "
 map("n", "<leader>x", ":bd<CR>", opts)           -- Close buffer
 map("n", "<leader>q", ":wq<CR>", opts)           -- Quit Vim
 map("n", "<leader>w", ":w<CR>", opts)            -- Save file
+map("n", "J", "5jzz", opts)                  -- Move down 5 lines and center
+map("n", "K", "5kzz", opts)                  -- Move up 5 lines and center
 
 -- Buffer navigation using Tab and Shift-Tab
 map("n", "<Tab>", ":bnext<CR>", opts)
 map("n", "<S-Tab>", ":bprevious<CR>", opts)
-map("n", "<leader>bd", ":bdelete<CR>", opts)
 
 -- Telescope mappings
 map("n", "<leader>f", ":Telescope find_files<CR>", opts)   -- Fuzzy search git files
 map('n', '<leader>o', "<cmd>lua require('telescope.builtin').oldfiles()<CR>", opts)   -- Open old files
-map("n", "<leader>w", ":Telescope live_grep<CR>", opts)    -- Fuzzy search with live grep
+map("n", "<leader>g", ":Telescope live_grep<CR>", opts)    -- Fuzzy search with live grep
 map("n", "<leader>z", ":Telescope current_buffer_fuzzy_find<CR>", opts)
-map("n", "<leader>b", ":Telescope file_browser<CR>", opts)
-map("n", "<leader>c", ":ChatGPT<CR>", opts)    -- ChatGPT
 
 -- LSP mappings
 map("n", "gd", ":Telescope lsp_definitions<CR>", opts)   -- Go to definition with Telescope
@@ -34,9 +33,7 @@ map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)   -- Rename sy
 map("n", "<leader>k", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)    -- Show hover info
 
 -- Quick fix and diagnostics
-map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)    -- Go to previous diagnostic
-map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)    -- Go to next diagnostic
-map("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", opts)    -- Telescope diagnostics view
+map("n", "<leader>d", "<cmd>Telescope diagnostics<CR>", opts)    -- Telescope diagnostics view
 
 -- Copilot mappings
 map("i", "<C-l>", "<cmd>lua vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')<CR>", opts)
