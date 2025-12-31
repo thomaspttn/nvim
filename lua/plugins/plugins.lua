c5a97ebd9d66d87752d201a63daae0287edd80ff
Thomas Patton <tjpatton1@gmail.com>
2025-12-31 13:06:16 -0500

    let's try

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -39,7 +39,7 @@ return {
           settings = {
             ["rust-analyzer"] = {
               completion = {
-                snippets = "none"  -- Disable snippets entirely
+                snippets = "none" -- Disable snippets entirely
               }
             }
           }
@@ -142,7 +142,7 @@ return {
           ["<CR>"] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
         },
         sources = cmp.config.sources({
-          { 
+          {
             name = "nvim_lsp",
             -- NO SNIPPETS
             entry_filter = function(entry, ctx)
@@ -286,4 +286,31 @@ return {
       vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
     end,
   },
+
+  {
+    'dmtrKovalenko/fff.nvim',
+    build = function()
+      -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+      -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+      require("fff.download").download_or_build_binary()
+    end,
+    -- if you are using nixos
+    -- build = "nix run .#release",
+    opts = {                -- (optional)
+      debug = {
+        enabled = true,     -- we expect your collaboration at least during the beta
+        show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+      },
+    },
+    -- No need to lazy-load with lazy.nvim.
+    -- This plugin initializes itself lazily.
+    lazy = false,
+    keys = {
+      {
+        "ff", -- try it if you didn't it is a banger keybinding for a picker
+        function() require('fff').find_files() end,
+        desc = 'FFFind files',
+      }
+    }
+  }
 }
